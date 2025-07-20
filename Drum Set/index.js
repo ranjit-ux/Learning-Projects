2eaff8a3299885a57aa0a3f884f29a049d75cb14
Ranjit Kumar Singh <ranjitksingh.079@gmail.com>
2025-07-20 11:12:48 +0530

    Drum Set with keyboard press recognition

---
+++ Drum Set/index.js
@@ -0,0 +1,89 @@
+let drumlen = document.querySelectorAll(".drum").length;
+
+for(let i=0;i<drumlen;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",(evt)=>{
+        var buttontext = evt.target.innerText;
+
+        switch(buttontext){
+            case "f":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                
+            default: console.log("NA");
+        }
+
+        buttonAnimation(buttontext);
+    })
+}
+
+    document.addEventListener("keydown",(event)=>{
+        var keypressed = event.key;
+        switch(keypressed){
+            case "f":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+            break;
+                
+            default: console.log("NA");
+        }
+
+        buttonAnimation(keypressed);
+    })
+
+function buttonAnimation(buttonpressed){
+    var currbutton = document.querySelector("."+buttonpressed);
+
+    currbutton.classList.add("pressed");
+    setTimeout(()=>{
+        currbutton.classList.remove("pressed");
+    },100);
+
+}
